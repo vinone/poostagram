2e0f2fa73f34a061ef518a873ad7443bf4a0da80
Raphael Melo <raphaelgmelo@gmail.com>
2012-12-02 16:26:51 -0200

    interface

--- src/ui/scripts/poostagram.js
+++ src/ui/scripts/poostagram.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 (function( $ ){
 
@@ -105,4 +106,123 @@ $(document).ready(function(){
 //	
 
 
+=======
+
+(function( $ ){
+
+  $.fn.resizeImage = function( options ) {  
+  
+    return this.each(function() {
+        var maxWidth = 350; // Max width for the image
+        var maxHeight = 350;    // Max height for the image
+        var ratio = 0;  // Used for aspect ratio
+        var width = $(this).width();    // Current image width
+        var height = $(this).height();  // Current image height
+
+        // Check if the current width is larger than the max
+        if(width > maxWidth){
+            ratio = maxWidth / width;   // get ratio for scaling image
+            $(this).css("width", maxWidth); // Set new width
+            $(this).css("height", height * ratio);  // Scale height based on ratio
+            height = height * ratio;    // Reset height to match scaled image
+            width = width * ratio;    // Reset width to match scaled image
+        }
+
+        // Check if current height is larger than max
+        if(height > maxHeight){
+            ratio = maxHeight / height; // get ratio for scaling image
+            $(this).css("height", maxHeight);   // Set new height
+            $(this).css("width", width * ratio);    // Scale width based on ratio
+            width = width * ratio;    // Reset width to match scaled image
+        }
+        
+        $(this).addClass('resized');
+        
+    });
+
+  };
+})( jQuery );
+
+
+
+
+
+function loadMorePoos(){
+
+	var lastPooId = 1;
+	var toAppend = [];
+	var i = 0;
+	
+	poostagram.getMorePoos(lastPooId, function(poos){
+	
+			for (var item in poos) {
+
+				toAppend[i++] = '<li poostId="';
+				toAppend[i++] = poos[item].id;
+				toAppend[i++] = '"><div class="paper-piece"><h1>';
+				toAppend[i++] = poos[item].title;
+				toAppend[i++] = '</h1><h2>';
+				toAppend[i++] = poos[item].author;
+				toAppend[i++] = '</h2><div class="photo"><img src="';
+				toAppend[i++] = poos[item].src;
+				toAppend[i++] = '" /><div class="photo-buttons"><a href="#">Não é cocô!</a></div></div></div></li>';
+
+			}
+	
+			$('#toilet-paper').append(toAppend.join(''));
+			
+			$('.photo img:not(resized)').each(function() {
+			    $(this).resizeImage();
+			});
+			
+			$('#toilet-paper').append($('#paper-footer'));
+			
+			//window.scrollTo(0, document.body.scrollHeight);
+			
+	
+	});
+
+}
+
+$.ajaxSetup({
+  error:function(xhr,status,code){
+  		alert(xhr.responseText);
+  }
+});
+
+
+$('#refresh').click(function(){
+
+		$('#refresh').hide();
+		$('#refresh').slideDown("slow");
+});
+
+$(document).ready(function(){	
+	
+	$('#refresh').hide();
+	$('#refresh').slideDown("slow");
+	
+	$('#load-more').click(function(){
+		loadMorePoos();
+	});
+	
+	$('.photo img:not(resized)').each(function() {
+	    $(this).resizeImage();
+	});
+	
+	$('#upload-form').ajaxForm(function(data){
+		 alert(data.message);
+	});	
+	
+//	$(window).scroll(function () { 
+//	    
+//	    $('#output').text("You've scrolled " + $(window).scrollTop() + " pixels");
+//	    
+//	    $('#paper').css('background-position-y', $(window).scrollTop());
+//	    
+//	});
+//	
+
+
+>>>>>>> interface
 });
